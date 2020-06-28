185a655726e1160ef3c8ed5f9752d96676204a72
Gyan <gyankr20@gmail.com>
2020-06-28 12:36:44 +0530

    ADD STATE IN CARTITEM

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,19 +1,34 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+
+
     render() {
-        return(
+        const { price, title, qty } = this.state;
+        return (
             <div className="cart-item">
                 <div className="left-block">
-                    <img style={styles.image}/>
+                    <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                <div style={{fontSize:25}}>phone</div>
-                <div style={{color:'#777'}}>Rs 9999</div>
-                <div style={{color:'#777'}}>Qty:1</div>
-                <div className="cart-item-actions">
-                    {/*Buttons*/}
-                </div>
+                    <div style={{ fontSize: 25 }}>{title}</div>
+                    <div style={{ color: '#777' }}>Rs {price}</div>
+                    <div style={{ color: '#777' }}>Qty:{qty}</div>
+                    <div className="cart-item-actions">
+                        {/*Buttons*/}
+                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/1828/1828817.svg" />
+                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/1264/1264407.svg" />
+                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/833/833520.svg" />
+                    </div>
                 </div>
             </div>
         );
@@ -22,11 +37,11 @@ class CartItem extends React.Component {
 
 const styles = {
 
-    image:{
-        height:110,
-        width:110,
-        borderRadius:4,
-        background:'#ccc'
+    image: {
+        height: 110,
+        width: 110,
+        borderRadius: 4,
+        background: '#ccc'
     }
 }
 
