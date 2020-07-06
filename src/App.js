9a759716a7f411345972f4053e896f829af04a01
Gyan <gyankr20@gmail.com>
2020-07-06 22:10:47 +0530

    Read Data from Firebase 2

--- src/App.js
+++ src/App.js
@@ -1,44 +1,36 @@
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import * as firebase from 'firebase';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: [
-        {
-          price: 64999,
-          title: 'TV',
-          qty: 0,
-          img: 'http://3.bp.blogspot.com/-A_c-e1o_iks/T5YmUTGikxI/AAAAAAAAB58/bUgyaa6XQls/s640/tv.jpg',
-          id: 1
-        },
-        {
-          price: 12999,
-          title: 'Mobile Phone',
-          qty: 0,
-          img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6E73YoleOXNCryjuNTA8Wq7IDJ__u-InmUg&usqp=CAU',
-          id: 2
-        },
-        {
-          price: 54099,
-          title: 'Laptop',
-          qty: 0,
-          img: 'https://previews.123rf.com/images/larryrains/larryrains1901/larryrains190100046/118556728-laptop-computer-a-vector-cartoon-illustration-of-a-laptop-computer-.jpg',
-          id: 3
-        },
-        {
-          price: 8999,
-          title: 'Ipod',
-          qty: 0,
-          img: 'https://clipartstation.com/wp-content/uploads/2017/11/mp3-player-clipart-6.jpg',
-          id: 4
-        }
-
-      ]
+      products: [],
+      loading:true
     }
   }
+
+  componentDidMount() {
+    firebase
+      .firestore()
+      .collection('products')
+      .get()
+      .then((snapshot) => {
+        const products = snapshot.docs.map((doc) => {
+          const data = doc.data();
+          data['id'] = doc.id;          
+          return data;
+        })
+        this.setState({
+          products,
+          loading:false
+        })
+
+      })
+  }
+
   handelIncreaseQuantity = (product) => {
     const { products } = this.state;
     const index = products.indexOf(product);
@@ -86,7 +78,7 @@ class App extends React.Component {
     return cartTotal;
   }
   render() {
-    const { products } = this.state;
+    const { products,loading } = this.state;
     return (
       <div className="App">
         <Navbar count={this.getCartCount()} />
@@ -96,7 +88,8 @@ class App extends React.Component {
           onDecreaseQuantity={this.handelDecreaseQuantity}
           onDeleteProduct={this.handelDeleteProduct}
         />
-        <div style={ {padding: 15, fontSize: 20} }>TOTAL: {this.getCartTotal()} </div>
+        {loading&& <h1>Loading Products . . .</h1>}
+        <div style={{ padding: 15, fontSize: 20 }}>TOTAL: {this.getCartTotal()} </div>
       </div>
     );
   }
